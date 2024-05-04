cba12b0dc92897c355617c590671e3782023869e
kiliman <kiliman@gmail.com>
2024-05-04 13:03:40 -0400

    Configure http2 server for Vite dev server

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,24 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { expressDevServer } from 'remix-express-dev-server'
 import envOnly from 'vite-env-only'
+import fs from 'fs'
+import path from 'path'
 
 installGlobals({ nativeFetch: true })
 
 export default defineConfig({
   build: {
     target: 'esnext',
+  },
+  server: {
+    https: {
+      key: fs.readFileSync(
+        path.resolve(path.join(process.cwd(), 'other/localhost-key.pem')),
+      ),
+      cert: fs.readFileSync(
+        path.resolve(path.join(process.cwd(), 'other/localhost-cert.pem')),
+      ),
+    },
   },
   plugins: [
     expressDevServer(),
